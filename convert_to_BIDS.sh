3f528a154a9f0df9b2a25686ca7ae21bff1d33e2
Mona Garvert <mona.garvert@gmail.com>
2019-07-16 19:13:24 +0200

    Adapted for multiple fieldmaps

--- convert_to_BIDS.sh
+++ convert_to_BIDS.sh
@@ -10,11 +10,16 @@ session=3 # edit
 ecmdir=/scr/mrincoming/33956.5c_20190705_114330.SKYRA # edit
 choiceNo=6 #edit
 objScanNo=(7 10 11) # edit
+
 # if session 2:
-separateAnatScan=0  # edit 	1:true, 0:false
+separateAnatScan=false  # true / false
 anatfile= #31331.0c/31331.0c_190228_104042_S3_MPRAGE_ADNI_WE_p2
 
-#29121.94/29121.94_180926_113130_S12_t1_mprage_ADNI_32Ch # 32059.82/32059.82_180109_074024_S9_t1_MPRAGE_ADNI_32 #31208.b5_180216_090704_S43_t1_mprage_ADNI_32Ch_fc   # edit
+# If multipel fieldmaps were acquired because the scan was interrupted this needs to be filled in. If not, leave multipleFieldmaps as false, rest empty
+multipleFieldmaps=false
+fieldmapIDs= ##(7 10)
+normInv= ##(4 8)
+
 
 
 ####Defining pathways
@@ -37,7 +42,6 @@ mkdir ${niidir}/sub-${subj}/ses-${session}/fmap
 scp -r ${ecmdir}/param* ${niidir}/sub-${subj}/ses-${session}/temp
 
 ###Convert dcm to nii
-#Only convert the Dicom folder anat
 dcm2niix -o ${niidir}/sub-${subj}/ses-${session}/temp ${ecmdir}/DICOM
 
 
@@ -45,35 +49,77 @@ dcm2niix -o ${niidir}/sub-${subj}/ses-${session}/temp ${ecmdir}/DICOM
 # Remove pilot scans
 rm -f *Pilot*
 
+if [[ $multipleFieldmaps ]];  
+then 
+	
+	for fmap in 1 2; # Two fieldmaps acquired	
+	do 
+	
+		for i in 1 2; 	# Two magnitude images
+		do
+			cp *gre_field_*_$(printf "%d" $[${fieldmapIDs[$fmap-1]}])_e${i}.nii ../fmap/sub-${subj}_ses-${session}_run-${fmap}_magnitude${i}.nii
+			cp *gre_field_*_$(printf "%d" $[${fieldmapIDs[$fmap-1]}])_e${i}.json ../fmap/sub-${subj}_ses-${session}_run-${fmap}_magnitude${i}.json
+		done
+		
+		cp *gre_field_*_$(printf "%d" $[$(( ${fieldmapIDs[$fmap-1]} + 1 ))])_ph.nii ../fmap/sub-${subj}_ses-${session}_run-${fmap}_phasediff.nii
+		cp *gre_field_*_$(printf "%d" $[$(( ${fieldmapIDs[$fmap-1]} + 1 ))])_ph.json ../fmap/sub-${subj}_ses-${session}_run-${fmap}_phasediff.json
+		sed -i '$s/}/,\n"EchoTime1": 0.00592}/' ../fmap/sub-${subj}_ses-${session}_run-${fmap}_phasediff.json
+		sed -i '$s/}/,\n"EchoTime2": 0.00838}/' ../fmap/sub-${subj}_ses-${session}_run-${fmap}_phasediff.json
+		
+		
+		# Multiple phase encoded directions
+		cp *norm*_$(printf "%d" $[${normInv[$fmap-1]}]).nii ../fmap/sub-${subj}_ses-${session}_run-${fmap}_dir-norm_epi.nii
+		cp *norm*_$(printf "%d" $[${normInv[$fmap-1]}]).json ../fmap/sub-${subj}_ses-${session}_run-${run}_dir-norm_epi.json
+		cp *invpol*_$(printf "%d" $[$(( ${normInv[$fmap-1]} + 1 ))]).nii ../fmap/sub-${subj}_ses-${session}_run-${fmap}_dir-invpol_epi.nii
+		cp *invpol*_$(printf "%d" $[$(( ${normInv[$fmap-1]} + 1 ))]).json ../fmap/sub-${subj}_ses-${session}_run-${fmap}_dir-invpol_epi.json
+		
+	done
+	
+	######  THE BIT BELOW HAS TO BE UPDATED APPROPRIATELY! THE WAY IT'S CODED NOW FIELDMAP 1 IS USED FOR BLOCKS 1 AND 2, FIELDMAP 2 IS USED FOR BLOCK 3.
+	
+	fmap=1
+	## Add intended for field:
+	sed -i '$s@}@,\n"IntendedFor":["ses-XXSESSXX/func/sub-XXSUBJXX_ses-XXSESSXX_task-object_run-01_bold.nii.gz",\n"ses-XXSESSXX/func/sub-XXSUBJXX_ses-XXSESSXX_task-object_run-02_bold.nii.gz"]}@' ${niidir}/sub-${subj}/ses-${session}/fmap/sub-${subj}_ses-${session}_run-${fmap}_phasediff.json
+	sed -i -e "s/XXSUBJXX/${subj}/g" ${niidir}/sub-${subj}/ses-${session}/fmap/sub-${subj}_ses-${session}_run-${fmap}_phasediff.json
+	sed -i -e "s/XXSESSXX/${session}/g" ${niidir}/sub-${subj}/ses-${session}/fmap/sub-${subj}_ses-${session}_run-${fmap}_phasediff.json
+	
+	fmap=2
+	## Add intended for field:
+	sed -i '$s@}@,\n"IntendedFor":["ses-XXSESSXX/func/sub-XXSUBJXX_ses-XXSESSXX_task-object_run-03_bold.nii.gz""]}@' ${niidir}/sub-${subj}/ses-${session}/fmap/sub-${subj}_ses-${session}_run-${fmap}_phasediff.json
+	sed -i -e "s/XXSUBJXX/${subj}/g" ${niidir}/sub-${subj}/ses-${session}/fmap/sub-${subj}_ses-${session}_run-${fmap}_phasediff.json
+	sed -i -e "s/XXSESSXX/${session}/g" ${niidir}/sub-${subj}/ses-${session}/fmap/sub-${subj}_ses-${session}_run-${fmap}_phasediff.json
+	
+else
 
-## Fieldmap
-for i in 1 2; do
-	cp *gre_field_*e${i}.nii ../fmap/sub-${subj}_ses-${session}_magnitude${i}.nii
-	cp *gre_field_*e${i}.json ../fmap/sub-${subj}_ses-${session}_magnitude${i}.json
+	## Fieldmap
+	for i in 1 2; do
+		cp *gre_field_*e${i}.nii ../fmap/sub-${subj}_ses-${session}_magnitude${i}.nii
+		cp *gre_field_*e${i}.json ../fmap/sub-${subj}_ses-${session}_magnitude${i}.json
+	done
+	cp *gre_field_*_ph.nii ../fmap/sub-${subj}_ses-${session}_phasediff.nii
+	cp *gre_field_*_ph.json ../fmap/sub-${subj}_ses-${session}_phasediff.json
+	sed -i '$s/}/,\n"EchoTime1": 0.00592}/' ../fmap/sub-${subj}_ses-${session}_phasediff.json
+	sed -i '$s/}/,\n"EchoTime2": 0.00838}/' ../fmap/sub-${subj}_ses-${session}_phasediff.json
+
+	## Add intended for field:
+	sed -i '$s@}@,\n"IntendedFor":["ses-XXSESSXX/func/sub-XXSUBJXX_ses-XXSESSXX_task-object_run-01_bold.nii.gz",\n"ses-XXSESSXX/func/sub-XXSUBJXX_ses-XXSESSXX_task-object_run-02_bold.nii.gz", \n"ses-XXSESSXX/func/sub-XXSUBJXX_ses-XXSESSXX_task-object_run-03_bold.nii.gz"]}@' ${niidir}/sub-${subj}/ses-${session}/fmap/sub-${subj}_ses-${session}_phasediff.json
+	sed -i -e "s/XXSUBJXX/${subj}/g" ${niidir}/sub-${subj}/ses-${session}/fmap/sub-${subj}_ses-${session}_phasediff.json
+	sed -i -e "s/XXSESSXX/${session}/g" ${niidir}/sub-${subj}/ses-${session}/fmap/sub-${subj}_ses-${session}_phasediff.json
+
+	# Multiple phase encoded directions
+	cp *norm*.nii ../fmap/sub-${subj}_ses-${session}_dir-norm_epi.nii
+	cp *norm*.json ../fmap/sub-${subj}_ses-${session}_dir-norm_epi.json
+	cp *invpol*.nii ../fmap/sub-${subj}_ses-${session}_dir-invpol_epi.nii
+	cp *invpol*.json ../fmap/sub-${subj}_ses-${session}_dir-invpol_epi.json
+
 done
-cp *gre_field_*_ph.nii ../fmap/sub-${subj}_ses-${session}_phasediff.nii
-cp *gre_field_*_ph.json ../fmap/sub-${subj}_ses-${session}_phasediff.json
-sed -i '$s/}/,\n"EchoTime1": 0.00592}/' ../fmap/sub-${subj}_ses-${session}_phasediff.json
-sed -i '$s/}/,\n"EchoTime2": 0.00838}/' ../fmap/sub-${subj}_ses-${session}_phasediff.json
-
-## Add intended for field:
-sed -i '$s@}@,\n"IntendedFor":["ses-XXSESSXX/func/sub-XXSUBJXX_ses-XXSESSXX_task-object_run-01_bold.nii.gz",\n"ses-XXSESSXX/func/sub-XXSUBJXX_ses-XXSESSXX_task-object_run-02_bold.nii.gz", \n"ses-XXSESSXX/func/sub-XXSUBJXX_ses-XXSESSXX_task-object_run-03_bold.nii.gz"]}@' ${niidir}/sub-${subj}/ses-${session}/fmap/sub-${subj}_ses-${session}_phasediff.json
-sed -i -e "s/XXSUBJXX/${subj}/g" ${niidir}/sub-${subj}/ses-${session}/fmap/sub-${subj}_ses-${session}_phasediff.json
-sed -i -e "s/XXSESSXX/${session}/g" ${niidir}/sub-${subj}/ses-${session}/fmap/sub-${subj}_ses-${session}_phasediff.json
-
-
-# Multiple phase encoded directions
-cp *norm*.nii ../fmap/sub-${subj}_ses-${session}_dir-norm_epi.nii
-cp *norm*.json ../fmap/sub-${subj}_ses-${session}_dir-norm_epi.json
-cp *invpol*.nii ../fmap/sub-${subj}_ses-${session}_dir-invpol_epi.nii
-cp *invpol*.json ../fmap/sub-${subj}_ses-${session}_dir-invpol_epi.json
 
 
 ## Anatomical data
 # Copy anatomy scans over if these are taken from a previous scanning session
 
 if (( $session == 2 )); then
-	if (( $separateAnatScan == 1 )) ; then
+	if [[ $separateAnatScan ]] ; then
 		echo Copying preexisting anatomical scan
 		 
 		mkdir anatomy
@@ -104,8 +150,6 @@ mkdir -p ${niidir}/sub-${subj}/ses-${session}/func
 
 #Rename func files
 #Break the func down into each task
-#Example filename: 2475376_TfMRI_visualCheckerboard_645_CHECKERBOARD_645_RR
-#BIDS filename: sub-2475376_ses-1_task-Checkerboard_acq-TR645_bold
 
 for run in 1 2 3;
 do
